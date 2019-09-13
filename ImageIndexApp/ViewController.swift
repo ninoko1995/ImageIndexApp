95ee272f9a67921bb8b893b64ad1a4e528105a6b
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-09-13 20:28:00 +0900

    Initial Commit

---
+++ ImageIndexApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageIndexApp
+//
+//  Created by Kota Ninomiya on 2019/09/13.
+//  Copyright © 2019 Kota Ninomiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImageIndexAppTests/ImageIndexAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImageIndexAppTests.swift
+//  ImageIndexAppTests
+//
+//  Created by Kota Ninomiya on 2019/09/13.
+//  Copyright © 2019 Kota Ninomiya. All rights reserved.
+//
+
+import XCTest
+@testable import ImageIndexApp
+
+class ImageIndexAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
